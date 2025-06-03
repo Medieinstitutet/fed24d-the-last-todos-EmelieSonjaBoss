47eb72a7ab0b0612fd8df57a54b89d0ce39c92de
Emelie Boss <Emelie.boss@medieinstitutet.se>
2025-06-03 20:45:57 +0200

    Updates

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'todo-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// Fetch resources from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached response if found
+        if (response) {
+          return response;
+        }
+
+        // Clone the request because it can only be used once
+        const fetchRequest = event.request.clone();
+
+        // Try to fetch from network
+        return fetch(fetchRequest).then(response => {
+          // Check if response is valid
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response because it can only be used once
+          const responseToCache = response.clone();
+
+          // Add the response to cache
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+}); 
